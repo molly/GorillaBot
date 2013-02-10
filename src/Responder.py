365f5304194a71c57c32d09e18ebf5aa66cde9c8
Molly White <molly.white5@gmail.com>
2013-02-10 00:47:13 -0500

    Bedtime!
    Working on linking to articles

--- src/Responder.py
+++ src/Responder.py
@@ -36,15 +36,17 @@ class Responder(object):
             return self.message[1:]
         else:
             return False
-        
+               
     def check_command(self, command_message):
         if command_message[0][0] == "!":
             command = command_message[0][1:]
         else:
             command = command_message[0]
-        command = command.tolower()
+        command = command.lower()
         if command in self.command_list:
-            print("{} is a command!".format(command))
+            command = "self." + command + "({})".format(command_message[1:])
+            self.logger.info("Executing {}.".format(command))
+            exec(command)
         else:
             print("{} is not a command!".format(command))
             
@@ -55,9 +57,24 @@ class Responder(object):
                 return self.message[idx:]
         return False
     
+    def link(self, message):
+        if "[[" not in message[0]:
+            self.say("You formatted your link incorrectly! Please format the command as !link [[article]].")
+            return 0
+        end_index = ""
+        for idx, word in enumerate(self.message):
+            if "]]" in word:
+                end_index = idx
+        if type(end_index) != int:
+            self.say("You formatted your link incorrectly! Please format the command as !link [[article]].")
+            return 0
+        else:
+            self.say("Linking {}.").format(message[:end_index])
+    
     def parse_message(self, line):
-        sender_nick = re.search(":(.*?)!~", line[0])
-        if line[2] == self.bot_nick:
+        self.sender_nick = re.search(":(.*?)!~", line[0])
+        self.chan = line[2]
+        if self.chan == self.bot_nick:
             private = True
         else:
             private = False
@@ -84,4 +101,9 @@ class Responder(object):
                 if command:
                     self.logger.debug("Received message beginning with an exclamation mark: {}".format(command))
                     self.check_command(command)
-            
+                    
+    def say(self, message):
+        if self.chan == self.bot_nick:
+            self._GorillaConnection.private_message(self.sender_nick, message)
+        else:
+            self._GorillaConnection.private_message(self.chan, message)
