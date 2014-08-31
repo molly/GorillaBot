491dafd4efd9c49105d39705f8e6c90d48ef3f54
Molly White <molly.white5@gmail.com>
2014-08-31 14:27:52 -0400

    Dammit, 2 spaces isn't PEP-compliant

--- plugins/misc.py
+++ plugins/misc.py
@@ -17,98 +17,103 @@
 
 from plugins.util import admin, command
 
+
 @command("admins")
 def adminlist(m):
-  """Provide a list of current bot admins."""
-  cursor = m.bot.db_conn.cursor()
-  cursor.execute('''SELECT nick FROM users WHERE botop = 1''')
-  data = cursor.fetchall()
-  cursor.close()
-  ops = [x[0] for x in data]
-  if ops:
-    print(ops)
-    if len(ops) == 1:
-      m.bot.private_message(m.location, "My bot admin is " + ops[0] + ".")
+    """Provide a list of current bot admins."""
+    cursor = m.bot.db_conn.cursor()
+    cursor.execute('''SELECT nick FROM users WHERE botop = 1''')
+    data = cursor.fetchall()
+    cursor.close()
+    ops = [x[0] for x in data]
+    if ops:
+        print(ops)
+        if len(ops) == 1:
+            m.bot.private_message(m.location, "My bot admin is " + ops[0] + ".")
+        else:
+            m.bot.private_message(m.location, "My bot admins are " + ", ".join(ops) + ".")
     else:
-      m.bot.private_message(m.location, "My bot admins are " + ", ".join(ops) + ".")
-  else:
-    nick = m.bot.get_config("nick")
-    m.bot.private_message(m.location, "{0} has no master. {0} is a free bot.".format(nick))
+        nick = m.bot.get_config("nick")
+        m.bot.private_message(m.location, "{0} has no master. {0} is a free bot.".format(nick))
 
 
 @admin("set")
 def setcommand(m):
-  """Adjust or view the settings on a command."""
-  if len(m.line) > 4:
-    m.bot.private_message(m.location, 'Too many arguments. Use "!set setting value [#channel]".')
-    return
+    """Adjust or view the settings on a command."""
+    if len(m.line) > 4:
+        m.bot.private_message(m.location,
+                              'Too many arguments. Use "!set setting value [#channel]".')
+        return
 
-  # Get channel ID of channel from which command was sent, or from the specified channel if given.
-  if len(m.line) <= 3:
-    chan = m.location
-  elif len(m.line) == 4:
-    if m.line[3][0] != "#":
-      m.bot.private_message(m.location, 'Poorly-formatted command. '
-        'Use "!set setting value [#channel]".')
-      return
-    chan = m.line[3]
-  chan_id = m.bot.get_chan_id(chan)
-  if chan_id is None:
-    m.bot.private_message(m.location,
-      "Cannot access settings for {0}. Do I know about the channel?".format(chan))
-    return
+    # Get channel ID of channel from which command was sent, or from the specified channel if given.
+    if len(m.line) <= 3:
+        chan = m.location
+    elif len(m.line) == 4:
+        if m.line[3][0] != "#":
+            m.bot.private_message(m.location, 'Poorly-formatted command. '
+                                              'Use "!set setting value [#channel]".')
+            return
+        chan = m.line[3]
+    chan_id = m.bot.get_chan_id(chan)
+    if chan_id is None:
+        m.bot.private_message(m.location,
+                              "Cannot access settings for {0}. Do I know about the channel?".format(
+                                  chan))
+        return
 
-  # Respond to command
-  cursor = m.bot.db_conn.cursor()
-  if len(m.line) == 1:
-    cursor.execute('''SELECT setting, value FROM settings WHERE chan_id = ?''', (chan_id,))
-    data = cursor.fetchall()
-    if data == []:
-      m.bot.private_message(m.location, "Nothing has been set for {0}.".format(chan))
+    # Respond to command
+    cursor = m.bot.db_conn.cursor()
+    if len(m.line) == 1:
+        cursor.execute('''SELECT setting, value FROM settings WHERE chan_id = ?''', (chan_id,))
+        data = cursor.fetchall()
+        if data == []:
+            m.bot.private_message(m.location, "Nothing has been set for {0}.".format(chan))
+        else:
+            m.bot.private_message(m.location, (" ".join(
+                map(lambda setting: ('"{0}" is set to "{1}".'.format(setting[0], setting[1])),
+                    data))))
+    elif len(m.line) == 2:
+        cursor.execute('''SELECT value FROM settings WHERE chan_id = ? AND setting = ?''',
+                       (chan_id, m.line[1]))
+        data = cursor.fetchone()
+        if data is None:
+            m.bot.private_message(m.location,
+                                  '"{0}" has not been set for {1}.'.format(m.line[1], chan))
+        else:
+            m.bot.private_message(m.location,
+                                  '"{0}" set to "{1}" in {2}.'.format(m.line[1], data[0], chan))
     else:
-      m.bot.private_message(m.location,
-          (" ".join(map(lambda setting: ('"{0}" is set to "{1}".'
-                          .format(setting[0], setting[1])), data))))
-  elif len(m.line) == 2:
-    cursor.execute('''SELECT value FROM settings WHERE chan_id = ? AND setting = ?''',
-        (chan_id, m.line[1]))
-    data = cursor.fetchone()
-    if data is None:
-      m.bot.private_message(m.location, '"{0}" has not been set for {1}.'.format(m.line[1], chan))
-    else:
-      m.bot.private_message(m.location,
-        '"{0}" set to "{1}" in {2}.'.format(m.line[1], data[0], chan))
-  else:
-    setting = m.line[1].lower()
-    value = m.line[2].lower()
-    cursor.execute('''INSERT INTO settings VALUES (?, ?, ?)''', (setting, value, chan_id))
-    m.bot.db_conn.commit()
-    cursor.close
-    m.bot.logger.info('"{0}" set to "{1}" in {2} by {3}.'
-                      .format(setting, value, chan, m.sender))
-    m.bot.private_message(m.location,
-      '"{0}" set to "{1}" in {2}.'.format(setting, value, chan))
+        setting = m.line[1].lower()
+        value = m.line[2].lower()
+        cursor.execute('''INSERT INTO settings VALUES (?, ?, ?)''', (setting, value, chan_id))
+        m.bot.db_conn.commit()
+        cursor.close
+        m.bot.logger.info(
+            '"{0}" set to "{1}" in {2} by {3}.'.format(setting, value, chan, m.sender))
+        m.bot.private_message(m.location, '"{0}" set to "{1}" in {2}.'.format(setting, value, chan))
+
 
 @admin()
 def unset(m):
-  '''Unset a given setting.'''
-  if len(m.line) != 2 and not (len(m.line) == 3 and m.line[2][0] == "#"):
-      m.bot.private_message(m.location,
-        'Poorly-formatted command. Use "!unset setting [#channel]".')
-      return
-  chan = m.location if len(m.line) == 2 else m.line[2]
-  chan_id = m.bot.get_chan_id(chan)
-  if chan_id is None:
-    m.bot.private_message(m.location,
-      "Cannot unset setting for {0}. Do I know about the channel?".format(chan))
-    return
-  cursor = m.bot.db_conn.cursor()
-  cursor.execute('''DELETE FROM settings WHERE setting = ? AND chan_id = ?''',
-    (m.line[1], chan_id))
-  m.bot.db_conn.commit()
-  cursor.close()
-  if cursor.rowcount == 1:
-    m.bot.private_message(m.location, '"{0}" unset for {1}.'.format(m.line[1], chan))
-  else:
-    m.bot.logger.info("Couldn't unset {0} for {1}.".format(m.line[1], chan))
-    m.bot.private_message(m.location, "Cannot unset {0} for {1}.".format(m.line[1], chan))
+    '''Unset a given setting.'''
+    if len(m.line) != 2 and not (len(m.line) == 3 and m.line[2][0] == "#"):
+        m.bot.private_message(m.location,
+                              'Poorly-formatted command. Use "!unset setting [#channel]".')
+        return
+    chan = m.location if len(m.line) == 2 else m.line[2]
+    chan_id = m.bot.get_chan_id(chan)
+    if chan_id is None:
+        m.bot.private_message(m.location,
+                              "Cannot unset setting for {0}. Do I know about the channel?".format(
+                                  chan))
+        return
+    cursor = m.bot.db_conn.cursor()
+    cursor.execute('''DELETE FROM settings WHERE setting = ? AND chan_id = ?''',
+                   (m.line[1], chan_id))
+    m.bot.db_conn.commit()
+    cursor.close()
+    if cursor.rowcount == 1:
+        m.bot.private_message(m.location, '"{0}" unset for {1}.'.format(m.line[1], chan))
+    else:
+        m.bot.logger.info("Couldn't unset {0} for {1}.".format(m.line[1], chan))
+        m.bot.private_message(m.location, "Cannot unset {0} for {1}.".format(m.line[1], chan))
