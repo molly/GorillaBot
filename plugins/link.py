45da02ff2cb800aad428391956616805a0a77584
Molly White <molly.white5@gmail.com>
2014-09-06 18:44:16 -0400

    Unescape HTML in links

--- plugins/link.py
+++ plugins/link.py
@@ -16,6 +16,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from plugins.util import command
+from html.parser import HTMLParser
 from urllib.request import Request, urlopen, URLError
 from urllib.parse import quote
 import json
@@ -41,8 +42,9 @@ def link(m, urls=None):
                 m.bot.logger.info("{0}: {1}".format(url, e.reason))
             else:
                 if match:
+                    h = HTMLParser()
                     m.bot.private_message(m.location, "Link: " +
-                                          re.sub('[\n\r\t]', ' ', match.group(1)))
+                                          h.unescape(re.sub('[\n\r\t]', ' ', match.group(1))))
 
 @command("relevantxkcd")
 def xkcd(m):
