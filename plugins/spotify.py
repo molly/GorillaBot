ce1395db9340ee694ef7a7c35d7d185e4319cf4e
Molly White <molly.white5@gmail.com>
2014-09-09 20:58:29 -0400

    Change formatting of response, add URL

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -11,16 +11,19 @@ def spotify(m):
     for spotify_uri in spotify_uris:
         try:
             type, id = _parse_spotify_uri(spotify_uri)
+        except ValueError:
+            m.bot.logger.error("Invalid Spotify URI: " + spotify_uri)
+        else:
             req = get_url(m, ENDPOINT.format(type, id))
             if req:
                 blob = json.loads(req)
                 if type == "track" or type == "album":
-                    m.bot.private_message(m.location,
-                                          blob["artists"][0]["name"] + " - " + blob["name"])
+                    m.bot.private_message(m.location, '"{0}" by {1} - {2}'
+                                          .format(blob["name"], blob["artists"][0]["name"],
+                                                  blob["external_urls"]["spotify"]))
                 else:
-                    m.bot.private_message(m.location, blob["name"])
-        except ValueError:
-            m.bot.logger.error("Invalid Spotify URI: " + spotify_uri)
+                    m.bot.private_message(m.location, "{0} - {1}"
+                                          .format(blob["name"], blob["external_urls"]["spotify"]))
 
 def _parse_spotify_uri(s):
     [type, id] = s.split(':')
