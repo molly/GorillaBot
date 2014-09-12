057c527ab0dd1633b72be1c4bed46579f43f6950
William Cummings <wcummings@hubspot.com>
2014-09-12 10:18:14 -0400

    Use bs4

--- plugins/secondst.py
+++ plugins/secondst.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import requests
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 from plugins.util import command
 
 URL = 'http://www.2ndstcafe.com/'
@@ -12,10 +12,10 @@ def secondst(m):
         m.bot.private_message(m.location, "Received non-200 status code: %d" % r.status_code)
         return
     soup = BeautifulSoup(r.text)
-    items = map(lambda x: x.text, soup.findAll('li', {'class': None, 'id': None}))
+    items = map(lambda x: x.text, soup.find_all('li', {'class': None, 'id': None}))
     for item in items:
         if is_veggie(item):
-            m.bot.private_message(m.location, "* %s" * % item)
+            m.bot.private_message(m.location, "* %s *" % item)
         else:
             m.bot.private_message(m.location, item)
 
