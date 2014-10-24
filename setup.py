97bc42c0426e27efe90f4dc6bb78684fd65b3213
Molly White <molly.white5@gmail.com>
2014-10-23 23:32:49 -0400

    Rearrange

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from distutils.core import setup
 setup(
   name = 'gorillabot',
-  packages = ['gorillabot'], # this must be the same as the name above
+  packages = ['gorillabot'],
   version = '2.0',
   description = 'An easily-extensible IRC bot.',
+  license = 'MIT',
   author = 'Molly White',
   author_email = 'molly.white5@gmail.com',
   url = 'https://github.com/molly/GorillaBot',
   download_url = 'https://github.com/molly/gorillabot/tarball/2.0',
   keywords = ['irc', 'bot', 'freenode'],
   classifiers = [],
+  install_requires = [
+      'beautifulsoup4',
+  ]
 )
