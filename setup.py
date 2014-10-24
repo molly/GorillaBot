c5528763f8db258d46202e0ce0e7175cc1d069a3
Molly White <molly.white5@gmail.com>
2014-10-23 23:00:28 -0400

    Get package ready for pypi

--- setup.py
+++ setup.py
@@ -1,29 +1,13 @@
-# Copyright (c) 2013-2014 Molly White
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software
-# and associated documentation files (the "Software"), to deal in the Software without
-# restriction, including without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all copies or
-# substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-from setuptools import setup
-
-setup(name='GorillaBot',
-      version='2.0',
-      description='An easily-extensible IRC bot written in Python.',
-      url='https://github.com/molly/GorillaBot',
-      author='Molly White',
-      author_email='molly.white5@gmail.com',
-      license='MIT',
-      install_requires=[
-          'beautifulsoup4'
-      ])
+from distutils.core import setup
+setup(
+  name = 'gorillabot',
+  packages = ['gorillabot'], # this must be the same as the name above
+  version = '2.0',
+  description = 'An easily-extensible IRC bot.',
+  author = 'Molly White',
+  author_email = 'molly.white5@gmail.com',
+  url = 'https://github.com/molly/GorillaBot',
+  download_url = 'https://github.com/molly/gorillabot/tarball/2.0',
+  keywords = ['irc', 'bot', 'freenode'],
+  classifiers = [],
+)
