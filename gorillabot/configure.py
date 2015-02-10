b43e07bd932df8762a522d3c4c24d67963233b25
Molly White <molly.white5@gmail.com>
2015-02-10 04:41:55 -0500

    Change to using JSON config files instead of sqlite

--- gorillabot/configure.py
+++ gorillabot/configure.py
@@ -16,47 +16,43 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from getpass import getpass
+import json
 import logging
+import os
 import re
-import sqlite3
 
 
 class Configurator(object):
     """Handles the configuration settings in the database."""
 
     def __init__(self, db_conn):
-        self.db_conn = db_conn
+        self.base_path = os.path.dirname(os.path.abspath(__file__))
+        self.file_path = os.path.abspath(os.path.join(self.base_path, "..", "config.json"))
         self.logger = logging.getLogger("GorillaBot")
 
     def configure(self):
         """Provide the user with prompts to interact with the configuration of the bot."""
 
-        # If there's an existing channels table, set joined to false for all channels
-        try:
-            cursor = self.db_conn.cursor()
-            cursor.execute('''UPDATE channels SET joined = 0''')
-            self.db_conn.commit()
-            cursor.close()
-        except sqlite3.OperationalError:
-            pass
-
-        # Drop the users_to_channels table if it exists
-        cursor = self.db_conn.cursor()
-        cursor.execute('''DROP TABLE IF EXISTS users_to_channels''')
-        self.db_conn.commit()
-        cursor.close()
-
-        # Create all the tables that we need, if we need them
-        self.create_tables()
-
-        # Prompt user for input
         ans = ''
         while ans not in ["0", "1", "2", "3", "4"]:
             data = self.get_settings()
-            if data is not None and len(data) > 0:
+            if data is None:
+                # File is poorly formatted and user doesn't want to overwrite
+                return False
+            if not data:
+                # File contains no configs
+                ans = ''
+                while ans not in ["0", "1"]:
+                    print("\n[0] Create new configuration\n[1] Exit")
+                    ans = input("")
+                    if ans == "0":
+                        return self.create_new()
+                    elif ans == "1":
+                        return False
+            elif data is not None:
                 print("Existing configurations:")
-                for row in data:
-                    print('\t' + row[0])
+                for key in data.keys():
+                    print('\t' + key)
                 print("\n[0] Load configuration\n[1] Create new configuration\n[2] View "
                       "configuration\n"
                       "[3] Remove configuration\n[4] Exit")
@@ -66,245 +62,113 @@ class Configurator(object):
                 elif ans == "1":
                     return self.create_new()
                 elif ans == "2":
-                    self.view()
+                    self.display()
                 elif ans == "3":
                     self.delete()
                 elif ans == "4":
                     return False
                 ans = ''
-            else:
-                ans = ''
-                while ans not in ["0", "1"]:
-                    print("\n[0] Create new configuration\n[1] Exit")
-                    ans = input("")
-                    if ans == "0":
-                        return self.create_new()
-                    elif ans == "1":
-                        return False
-
-    def create_tables(self):
-        cursor = self.db_conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS configs
-                (name TEXT NOT NULL PRIMARY KEY,
-                 nick TEXT NOT NULL,
-                 realname TEXT NOT NULL,
-                 ident TEXT NOT NULL,
-                 password TEXT,
-                 youtube TEXT,
-                 forecast TEXT)''')
-        cursor.execute('''CREATE TABLE IF NOT EXISTS channels
-              (chan_id INTEGER PRIMARY KEY,
-               name TEXT NOT NULL,
-               joined BOOLEAN NOT NULL CHECK(joined IN(0,1)),
-               config TEXT,
-
-               FOREIGN KEY(config) REFERENCES configs(name) ON DELETE CASCADE,
-               UNIQUE(name, config) ON CONFLICT REPLACE)
-               ''')
-        cursor.execute('''CREATE TABLE IF NOT EXISTS users
-              (user_id INTEGER PRIMARY KEY,
-               nick TEXT NOT NULL,
-               user TEXT,
-               host TEXT,
-               botop BOOLEAN NOT NULL CHECK(botop IN(0,1)),
-               config TEXT,
-               FOREIGN KEY(config) REFERENCES configs(name) ON DELETE CASCADE)
-               ''')
-        cursor.execute('''CREATE TABLE users_to_channels
-              (user_id INTEGER,
-               chan_id INTEGER,
-               FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-               FOREIGN KEY(chan_id) REFERENCES channels(chan_id) ON DELETE CASCADE)
-               ''')
-        cursor.execute('''CREATE TABLE IF NOT EXISTS settings
-              (setting TEXT,
-               value TEXT,
-               chan_id INTEGER,
-               FOREIGN KEY(chan_id) REFERENCES channels(chan_id) ON DELETE CASCADE,
-               UNIQUE(setting, chan_id) ON CONFLICT REPLACE)
-               ''')
-        self.db_conn.commit()
-        cursor.close()
 
     def get_settings(self):
-        """Retrieve existing configurations, or create the table if it does not exist."""
-        cursor = self.db_conn.cursor()
-        cursor.execute('''SELECT name FROM configs''')
-        data = cursor.fetchall()
-        cursor.close()
-        return data
+        """Retrieve existing configurations, or create the config file if it does not exist."""
+        try:
+            with open(self.file_path, "r") as f:
+                blob = json.load(f)
+        except FileNotFoundError:
+            self.logger.debug("No configuration file. Creating new one at {}."
+                              .format(self.file_path))
+            with open(self.file_path, "w") as f:
+                json.dump({}, f)
+                blob = {}
+        except ValueError:
+            overwrite = input("Configuration file is formatted incorrectly. Erase and create a new"
+                              " one? [y/n] ")
+            if overwrite.lower() == "y":
+                self.logger.info("Overwrote incorrectly-formatted configuration file.")
+                with open(self.file_path, "w") as f:
+                    json.dump({}, f)
+                    blob = {}
+            else:
+                return None
+        return blob
 
     def create_new(self):
         """Create a new configuration."""
-        verify = ''
-        while verify != 'y':
+        existing = self.get_settings()
+        verify = False
+        while not verify:
+            print(verify)
             print('\n')
             name = ""
             while name == "":
                 name = input("Unique name for this configuration: ")
-                cursor = self.db_conn.cursor()
-                cursor.execute('''SELECT * FROM configs WHERE name = ?''', (name,))
-                data = cursor.fetchone()
-                cursor.close()
-                if data:
+                if name in existing.keys():
                     print('The name "{0}" is not unique.'.format(name))
                     name = ""
-            nick = self.prompt("Nick", "GorillaBot")
-            realname = self.prompt("Ident", "GorillaBot")
-            ident = self.prompt("Realname", "GorillaBot")
-            chans = self.prompt("Channel(s)")
-            botop = self.prompt("Bot operator(s)", '')
-            password = self.prompt("Server password (optional)", hidden=True)
-            youtube = self.prompt("YouTube API key (optional)", hidden=True)
-            forecast = self.prompt("Forecast.io API key (optional)", hidden=True)
-            chans = re.split(',? ', chans)
-            botop = re.split(',? ', botop)
-            self.display((name, nick, realname, ident, password, youtube, forecast), chans, botop)
-            verify = input('Is this configuration correct? [y/n]: ').lower()
-        self.save_config((name, nick, realname, ident, chans, botop, password, youtube, forecast))
+            settings = {name: {}}
+            settings[name]["nick"] = self.prompt("Nick", "GorillaBot")
+            settings[name]["realname"] = self.prompt("Ident", "GorillaBot")
+            settings[name]["ident"] = self.prompt("Realname", "GorillaBot")
+            settings[name]["chans"] = re.split(',? ', self.prompt("Channel(s)"))
+            settings[name]["botops"] = re.split(',? ', self.prompt("Bot operator(s)", ''))
+            settings[name]["password"] = self.prompt("Server password (optional)", hidden=True)
+            settings[name]["youtube"] = self.prompt("YouTube API key (optional)", hidden=True)
+            settings[name]["forecast"] = self.prompt("Forecast.io API key (optional)", hidden=True)
+            verify = self.verify(settings, name)
+        new_settings = self.get_settings()
+        new_settings.update(settings)
+        self.save_config(new_settings)
         return name
 
     def load_settings(self):
-        """Show a given configuration, and allow the user to modify it if needed."""
-        while True:
-            name, data, channels, botops = self.view()
-            cursor = self.db_conn.cursor()
-            self.db_conn.commit()
-            cursor.close()
-            cursor = self.db_conn.cursor()
-            cursor.execute('''CREATE TABLE IF NOT EXISTS users
-                (user_id INTEGER PRIMARY KEY,
-                 nick TEXT NOT NULL,
-                 user TEXT,
-                 host TEXT,
-                 botop BOOLEAN NOT NULL CHECK(botop IN(0,1)),
-                 config TEXT,
-                 FOREIGN KEY(config) REFERENCES configs(name) ON DELETE CASCADE)
-                 ''')
-            cursor.execute('''DELETE FROM users WHERE config = ?''', (name,))
-            self.db_conn.commit()
-            cursor.close()
-            if botops != ['']:
-                cursor = self.db_conn.cursor()
-                for op in botops:
-                    cursor.execute('''INSERT INTO users VALUES (NULL, ?, NULL, NULL, 1, ?)''',
-                                   (op, name))
-                self.db_conn.commit()
-                cursor.close()
-            self.verify(data, channels, botops)
-            return name
+        """Show a given configuration."""
+        return self.display()
 
-    def view(self):
-        """Open a configuration for viewing."""
-        while True:
-            configuration = input("Please choose an existing configuration: ")
-            cursor = self.db_conn.cursor()
-            cursor.execute("SELECT * FROM configs WHERE name = ?", (configuration,))
-            data = cursor.fetchone()
-            cursor.close()
-            if data is not None:
-                cursor = self.db_conn.cursor()
-                cursor.execute('''SELECT name FROM channels WHERE config = ?''', (configuration,))
-                channels = cursor.fetchall()
-                cursor.close()
-                channels = [x[0] for x in channels]
-                cursor = self.db_conn.cursor()
-                cursor.execute('''SELECT nick FROM users WHERE botop = 1 AND config = ?''',
-                               (configuration,))
-                botops = cursor.fetchall()
-                cursor.close()
-                botops = [x[0] for x in botops]
-                self.display(data, channels, botops)
-                return (configuration, data, channels, botops)
-            else:
-                print("{0} is not the name of an existing configuration.".format(configuration))
-
-    def delete(self, name=None):
+    def delete(self):
         """Delete a configuration."""
-        if name is None:
-            name = input("Please choose an existing configuration: ")
-        cursor = self.db_conn.cursor()
-        cursor.execute("DELETE FROM configs WHERE name = ?", (name,))
-        self.db_conn.commit()
-        cursor.close()
+        settings = self.get_settings()
+        name = input("Please choose an existing configuration: ")
+        try:
+            del settings[name]
+            self.save_config(settings)
+        except KeyError:
+            print("No configuration named {}.".format(name))
 
-    def display(self, data, chans, botops):
+    def display(self, settings = None, name = None):
         """Display a configuration."""
-        chans = ", ".join(chans)
-        botops = ", ".join(botops)
-        password = "[hidden]" if data[4] else "[none]"
-        youtube = "[hidden]" if data[5] else "[none]"
-        forecast = "[hidden]" if data[6] else "[none]"
+        if not settings:
+            settings = self.get_settings()
+            name = ""
+            while name == "":
+                name = input("Please choose an existing configuration: ")
+                if name not in settings.keys():
+                    print("No configuration named {}.".format(name))
+                    name = ""
+        chans = ", ".join(settings[name]["chans"])
+        botops = ", ".join(settings[name]["botops"])
+        password = "[hidden]" if settings[name]["password"] else "[none]"
+        youtube = "[hidden]" if settings[name]["youtube"] else "[none]"
+        forecast = "[hidden]" if settings[name]["forecast"] else "[none]"
         print(
             "\n------------------------------\n Nickname: {0}\n Real name: {1}\n Identifier: {2}\n"
-            " Channels: {3}\n Bot operator(s): {4}\n Server password: {5}\nYouTube API key: {6}\n"
-            "Forecast.io API key: {7}\n------------------------------\n".format(data[1], data[2],
-                                                                                data[3], chans,
-                                                                                botops, password,
-                                                                                youtube, forecast))
+            " Channels: {3}\n Bot operator(s): {4}\n Server password: {5}\n YouTube API key: {6}\n"
+            " Forecast.io API key: {7}\n------------------------------\n"
+            .format(settings[name]["nick"], settings[name]["realname"], settings[name]["ident"],
+                    chans, botops, password, youtube, forecast))
+        return name
 
-    def verify(self, data, chans, botops):
+    def verify(self, settings, name):
         """Verify a configuration, and make changes if needed."""
-        verify = input('Is this configuration correct? [y/n]: ').lower()
-        if verify == 'y':
-            return
-        else:
-            verify = ''
-            while verify != 'y':
-                print('\n')
-                name = data[0]
-                nick = self.prompt("Nick", data[1])
-                realname = self.prompt("Ident", data[2])
-                ident = self.prompt("Realname", data[3])
-                chans = self.prompt("Chans", ", ".join(chans))
-                botop = self.prompt("Bot operator(s)", ", ".join(botops))
-                password = self.prompt("Server password (optional)", hidden=True)
-                youtube = self.prompt("YouTube API key (optional)", hidden=True)
-                forecast = self.prompt("Forecast.io API key (optional)", hidden=True)
-                chans = re.split(',? ', chans)
-                botop = re.split(',? ', botop)
-                self.display((name, nick, realname, ident, password, youtube, forecast), chans,
-                             botop)
-                verify = input('Is this configuration correct? [y/n]: ').lower()
-            self.delete(name)
-            cursor = self.db_conn.cursor()
-            cursor.execute('''DELETE FROM channels WHERE config = ?''', (name,))
-            cursor.execute('''DELETE FROM users WHERE config = ?''', (name,))
-            self.db_conn.commit()
-            cursor.close()
-            self.save_config(
-                (name, nick, realname, ident, chans, botop, password, youtube, forecast))
+        verify = ''
+        while verify != 'y' and verify != 'n':
+            self.display(settings, name)
+            verify = input('Is this configuration correct? [y/n]: ').lower()
+        return True if verify.lower() == 'y' else False
 
-    def save_config(self, data):
+    def save_config(self, new_settings):
         """Save changes to the configuration table."""
-        cursor = self.db_conn.cursor()
-        cursor.execute('''INSERT INTO configs VALUES (?, ?, ?, ?, ?, ?, ?)''',
-                       (data[0], data[1], data[2], data[3], data[6], data[7], data[8]))
-        self.db_conn.commit()
-        cursor.close()
-        if type(data[4]) is str:
-            channels = re.split(',? ', data[4])
-        else:
-            channels = data[4]
-        if type(data[5]) is str:
-            botops = re.split(',? ', data[5])
-        else:
-            botops = data[5]
-        cursor = self.db_conn.cursor()
-        if channels != ['']:
-            for chan in channels:
-                if chan[0] != "#":
-                    chan = "#" + chan
-                cursor.execute('''INSERT INTO channels VALUES (NULL, ?, 0, ?)''', (chan, data[0]))
-        self.db_conn.commit()
-        cursor.close()
-        cursor = self.db_conn.cursor()
-        if botops != ['']:
-            for op in botops:
-                cursor.execute('''INSERT INTO users VALUES (NULL, ?, NULL, NULL, 1, ?)''',
-                               (op, data[0]))
-        self.db_conn.commit()
-        cursor.close()
+        with open(self.file_path, "w") as f:
+            json.dump(new_settings, f, indent=4)
 
     def prompt(self, field, default=None, hidden=False):
         """Prompt a user for input, displaying a default value if one exists."""
